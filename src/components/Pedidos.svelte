99cf5aad97cef3c6d2fc55a6fbccc3afac743522
jualon92 <londrajuan@gmail.com>
2022-05-22 11:05:21 -0300

    estetica validacion pedido

--- src/components/Pedidos.svelte
+++ src/components/Pedidos.svelte
@@ -382,14 +382,38 @@ import { limitToLast } from "firebase/firestore";
 
     {#if mostrarPedido}
         <div class="contenedor-infoPedido">
+
+         
+
+            <div class="form-group">  
+            <fieldset>
+                <label class="form-label mt-4" for="readOnlyInput">Pedido</label>
+                <input class="form-control " id="readOnlyInput" type="text" placeholder={partePedida.nombre}  readonly >
+            
+                <label class="form-label mt-4" for="readOnlyInput">Precio</label>
+                <input class="form-control" id="readOnlyInput" type="text" placeholder={partePedida.precio}  readonly >
+            
+                <label class="form-label mt-4" for="readOnlyInput">Puntos disponibles</label>
+                <input class="form-control" id="readOnlyInput" type="text" placeholder={$Puntaje}  readonly >
+            
+                <label class="form-label mt-4" for="readOnlyInput">Puntos restantes</label>
+                <input class:is-invalid={ $Puntaje - partePedida.precio < 0} class:is-valid={$Puntaje - partePedida.precio > 0} class="form-control" id="readOnlyInput" type="text" placeholder={$Puntaje - partePedida.precio < 0 ? "Sin fondos suf" : $Puntaje - partePedida.precio} readonly >
+            
+                
+            </fieldset>
+          
+        </div>
+         <!--  
             <div class="pedido-info">Pedido: {partePedida.nombre}</div>
             <div class="pedido-info">Precio: {partePedida.precio}</div>
             <div class="pedido-info">Mis puntos: {$Puntaje}</div>
             <div class="pedido-info">
                 {$Puntaje - partePedida.precio > 0
                     ? "Total : " + ($Puntaje - partePedida.precio)
-                    : "sin Fondos suficientes"}
+                    : "sin Fondos suficientes"} 
+                    <label class="form-label mt-4" for="inputInvalid">Invalid input</label>
             </div>
+            -->
             <div class="botonera-pedido">
                 <button class="btn btn-primary">Volver atras</button>
                 <button
@@ -571,7 +595,9 @@ import { limitToLast } from "firebase/firestore";
             width: 90%;
         }
     }
-
+    ::placeholder{
+        color:black;
+    }
     .pedido-info {
         border-bottom-style: groove;
     }
